db678abcf6be9d5465056b8b7720755d828310da
Hajer K <hajerk@MacBookAir.lan>
2026-01-08 18:37:12 +0100

    task completed

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ Create a variable that stores the name of your cafe.
 Print out a greeting such as "Welcome to Technigo Cafe! What would you like to order today?"
 Where Technigo Cafe is replaced by the name of your cafe that is stored in the variable.
 */
+
 const cafeName = "Technigo Cafe";
 console.log(`Welcome to ${cafeName}! What would you like to order today?`);
 
@@ -29,6 +30,10 @@ Print out "You said this coffee is the best, that was actually true"
 Where true is replaced by your varible
 */
 
+const goodCoffee = true;
+
+console.log(`You said thais coffee is the best, thet was actually ${goodCoffee}!`);
+
 /*
 4)
 Create a variable called cafeGuests, that shows us how many cafeGuests we have.
@@ -38,6 +43,10 @@ Assign it a new value.
 Print it out. => This should give you the new value.
 */
 
+let cafeGuests = 5;
+console.log(cafeGuests);
+cafeGuests = 10;
+console.log(cafeGuests);
 /*
 5)
 Create a variable called maxGuests, that shows us how many guests are allowed in the cafe.
@@ -47,16 +56,30 @@ assign it a new value.
 Print it out. => This should give you an error because it shouldn't be able to be changed.
 */
 
+let maxGuests = 6;
+console.log(maxGuests);
+maxGuests = 5;
+console.log(maxGuests);
+
 /*
 6)
 Create a variable that stores a string.
 Print out that string in only UPPERCASE letters.
 */
 
+const myWord = "This is my first javascript words";
+
+console.log(myWord.toUpperCase());
+
 /*
 7)
 Print out the same string in only lowercase letters.
 */
+
+
+const newWord = "THIS IS NOT THE SAME TEXT !";
+
+console.log(newWord.toLowerCase());
 
 /*
 8) **BONUS**
@@ -65,3 +88,9 @@ Then figure out a way to replace the word "summer" in the string with the word "
 Should give you => "Today we have a special winter deal!"
 (check for a specific string method...)
 */
+
+const summerDeal = "Today we have a special summer deal!";
+
+console.log(summerDeal);
+
+console.log(summerDeal.replace("summer", "winter"));
